b372bc0fcffe09c0d7051c464c9223e86af33b4b
Renaud Lepage <root@cybikbase.com>
2023-07-02 18:10:17 -0700

    Autogen install path based on prefix location
    If the environment is autodetected as being from a known
    launcher (at the time of coding, Steam is the only one that
    is to be supported), generate an install location based on
    the detected prefix. This way, if/when installer chain-launching
    becomes a thing, the game will be installed in a C:-relative location.
    
    This is actually important because the Genshin, Houkai 3rd
    and HSR installers have an issue where they will mis-detect
    free space available on the target drive if the game is launched
    via "Z:", which is aliased to / and very much a weird thing
    for free-space detection. Auto-generating a path into the prefix,
    at least for Steam but maybe ultimately coming bac to it for our
    own installs as well, works around this WINE issue.

--- src/games/genshin/consts.rs
+++ src/games/genshin/consts.rs
@@ -1,5 +1,7 @@
 use std::path::PathBuf;
 
+use crate::games::common;
+
 pub const FOLDER_NAME: &str = "anime-game-launcher";
 
 lazy_static::lazy_static! {
@@ -10,6 +12,14 @@ lazy_static::lazy_static! {
         .ok()
         .and_then(|limit| limit.parse::<usize>().ok())
         .unwrap_or(8 * 1024 * 1024); // 8 MiB
+}
+
+/// Get assumed Steam Prefix install path
+///
+/// Generate a sane, possible, "relative to the prefix's C:\ root" install target for games that
+///  need such a location to install the game(s) in.
+pub fn base_game_install_dir() -> anyhow::Result<PathBuf> {
+    common::base_install_dir(launcher_dir().unwrap())
 }
 
 /// Get default launcher dir path
