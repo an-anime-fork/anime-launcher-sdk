9e5362f88cb625faefa37064e88131eefe7269f4
Renaud Lepage <root@cybikbase.com>
2025-06-30 18:23:05 -0700

    forked

--- src/games/zzz/config/schema/components.rs
+++ src/games/zzz/config/schema/components.rs
@@ -19,7 +19,7 @@ impl Default for Components {
         Self {
             path: launcher_dir.join("components"),
             servers: vec![
-                String::from("https://github.com/an-anime-team/components")
+                String::from("https://github.com/an-anime-fork/components")
             ]
         }
     }
