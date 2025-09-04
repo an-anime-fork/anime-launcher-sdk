6157ae37621cd7b7b69c4f2b1304dc5b8f45a5e8
Renaud Lepage <root@cybikbase.com>
2025-09-03 23:31:27 -0700

    little steam bit

--- src/traits/integrations/steamgame.rs
+++ src/traits/integrations/steamgame.rs
@@ -1,5 +1,9 @@
+use crate::integrations::steam;
 
 pub trait SteamGame {
     const STEAM_GAME_ID: i32 = -1;
-    fn has_steam_game_entry(&self) -> bool { false }
+    fn has_steam_game_entry() -> bool { false }
+
+    fn was_launched_from_steam_game() -> bool { steam::launched_from_steam() }
+
 }
