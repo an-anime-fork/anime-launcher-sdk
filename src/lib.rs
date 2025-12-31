ba7a004767ef34c30eb72998237299ef398557c9
Renaud Lepage <root@cybikbase.com>
2025-12-30 23:38:16 -0500

    Revert "feat: remove wuwa and pgr support"
    This reverts commit 5cab1e9666a6cb44002f71ed0aa061646f352998.

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,12 @@ pub use games::zzz;
 #[cfg(feature = "honkai")]
 pub use games::honkai;
 
+#[cfg(feature = "pgr")]
+pub use games::pgr;
+
+#[cfg(feature = "wuwa")]
+pub use games::wuwa;
+
 #[cfg(feature = "config")]
 pub mod config;
 
